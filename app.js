0ae575de8e633b886ed73b43e6bab0a2f79a59dc
Aniruddha <aniruddhaguchait13@gmail.com>
2026-04-01 21:28:40 +0530

    feat: connected mongodb atlas

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-if(process.env.NODE_ENV != "production"){
-    require('dotenv').config();
-};
+if (process.env.NODE_ENV != "production") {
+    require("dotenv").config();
+}
 
-const express= require("express");
-const app= express();
-const port= 3000;
-const mongoose= require("mongoose");
-const path= require("path");
-const ejsMate= require("ejs-mate");
-const ExpressError= require("./utils/ExpressError.js");
+const express = require("express");
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
+const path = require("path");
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError.js");
 const methodOverride = require("method-override");
 const session = require("express-session");
 const flash = require("connect-flash");
@@ -24,35 +24,45 @@ app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 
-const sessionOptions={
-  secret: 'mysecretcodethis',
+
+const MongoStore = require("connect-mongo").default;
+
+const dbUrl = process.env.ATLAS_DB_URL;
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+    secret: process.env.SESSION_SECRET,
+    },
+  touchAfter: 24 * 60 * 60,
+});
+
+store.on("error", (err) => {
+    console.log("Error in Session Store:", err);
+});
+
+const sessionOptions = {
+  store,
+  secret: process.env.SESSION_SECRET,
   resave: false,
-  saveUninitialized: true,
-  cookie:{
+  saveUninitialized: false,
+  cookie: {
     expires: Date.now() + 1000 * 60 * 60 * 24 * 14,
     maxAge: 1000 * 60 * 60 * 24 * 14,
     httpOnly: true,
-  }
+  },
 };
 
+async function main() {
+  await mongoose.connect(dbUrl);
+}
 main()
-    .then(()=>{
-        console.log("connected to DB");
-    })
-    .catch((err)=>{
-        console.log(err)
-    });
-async function main(){
-    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
-};
-
-// app.get("/", (req, res) => {
-//     res.send("Home Page");
-// });
+  .then(() => console.log("connected to DB"))
+  .catch((err) => console.log(err));
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -60,41 +70,29 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
-
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req, res, next)=>{
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
-    next();
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  next();
 });
-
-// app.get("/demouser", async (req, res)=>{
-//     let fakeUser = new User({
-//         email:"aniruddha@gmail.com",
-//         username:"sigmastudent"
-//     });
-
-//     let registeredUser = await User.register(fakeUser,"helloworld");
-//     res.send(registeredUser);
-// });
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
-//Invalid Routes
-app.use((req, res, next)=>{
-    next(new ExpressError(404, "Page not found!"))
+app.use((req, res, next) => {
+  next(new ExpressError(404, "Page not found!"));
 });
 
 app.use((err, req, res, next) => {
-    let {statusCode = 500, message = "Something went wrong"} = err;
-    res.status(statusCode).render("error", {message});
+  let { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).render("error", { message });
 });
 
 app.listen(port, () => {
-    console.log(`server is listening on ${port}`);
+  console.log(`server is listening on ${port}`);
 });
